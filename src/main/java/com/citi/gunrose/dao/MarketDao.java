153bde1a8044d6eab6bb696b60b47fd05cd03bd9
Quentin <1257592057@qq.com>
2017-08-10 14:28:53 +0800

    add sql

--- src/main/java/com/citi/gunrose/dao/MarketDao.java
+++ src/main/java/com/citi/gunrose/dao/MarketDao.java
@@ -2,8 +2,21 @@ package com.citi.gunrose.dao;
 
 import com.citi.gunrose.persistence.model.Market;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
 /**
  * Created by tangjing on 2017/8/8.
  */
 public interface MarketDao extends BaseDao<Market> {
+
+    public List<Object[]> queryMostPickedStocks();
+    public BigInteger queryStockNumber();
+    public List<Object[]> queryMostVolumeStocks();
+    public Integer queryVolumeNumber();
+    public List<Object[]> queryMostIncreaseStocks();
+    public BigDecimal queryIncreaseNumber();
+    public List<Object[]> queryMostDecreaseStocks();
+    public BigDecimal queryDecreaseNumber();
 }
